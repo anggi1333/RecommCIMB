de9bdc06ae07212949be7f4ac7fc7e07e2d5e55f
user <user@NDS-LPT-0080>
2021-02-18 10:18:33 +0700

    Commit 3

---
+++ Object Repository/1 Komitmen Audit/Response Approval/1 Detail.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>1 Detail</name>
+   <tag></tag>
+   <elementGuidId>646c276f-e7ce-4726-8429-1e583cc051e3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/form/div[1]/div[2]/div[3]/div[1]/div[2]/table[1]/tbody/tr/td[6]/div/div/div/div/a/span/span/span[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/1 Komitmen Audit/View/Page_/iframe_CAHS_nodeundefined_IFrame</value>
+   </webElementProperties>
+</WebElementEntity>
